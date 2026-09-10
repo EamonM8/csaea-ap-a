5d2a31e97c38640e741c747aac8aa2223522c154
EamonM8 <mckeon20282481@student.nhvweb.net>
2026-09-10 13:08:06 +0000

    Basic Class

---
+++ BasicClass.java
@@ -0,0 +1,17 @@
+public class BasicClass {
+
+    public static void main(String[] args) {
+        
+        // Attributes / data
+        // variables
+
+        // Constructor --- Special Method called every time you make an object
+        // Same name as the class, followed by parenthesis.
+        // BasicClass()
+
+        // behaviors / methods
+        // you can write as many as you want.
+
+
+    }
+}
